24a4d575610d4e87b16e2e6e9ed51126a9d68ad7
Rickh <hpx210650@gmail.com>
2026-09-24 00:09:26 +0800

    🎉 init: update structure

--- co-markdown-frontend/src/common/exception/api.exception.ts
+++ co-markdown-frontend/src/common/exception/api.exception.ts
@@ -1,15 +1,17 @@
 import { AppException } from './app.exception';
-import { ErrorCode } from '../../../shared/error-code.constant';
+import { ErrorCode } from '../../../../shared/error-code.constant';
 
 /**
  * 前端呼叫 API 錯誤
  */
 export class ApiException extends AppException {
+  public readonly code: ErrorCode;
   constructor(
-    public readonly code: ErrorCode,
+    code: ErrorCode,
     message?: string,
   ) {
     super(message);
+    this.code = code;
     this.name = 'ApiException';
   }
 }
